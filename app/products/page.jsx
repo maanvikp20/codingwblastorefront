edbc63bb4a405a8f0cde67abcebaf630385c8ad4
Bobbythecoder2034 <kkoala110@gmail.com>
2026-05-07 13:31:49 -0700

    page repair

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
+
+
 
 const Page = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="search-area">
+        <button className="search-button">Search</button>
+        <input type='search' className='search-bar' />
+        <select className='filter-bar' >
+          <option value="">Popular</option>
+          <option value="">Download</option>
+          <option value="">Print Count</option>
+          <option value="">Alphabetical</option>
+        </select>
+      </div>
+
+      <div className="products">
+        <div className='product'>
+          <div className="product-header">
+            <h1>Mini Submarine</h1>
+          </div>
+          <Image src="" alt="" className="product-img" />
+          <div className="product-footer"></div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+        
+    </>
+    
   )
 }
 
 export default Page
+
