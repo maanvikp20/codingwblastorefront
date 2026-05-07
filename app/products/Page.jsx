32807c5e3c5fe86dc4b9a9ef33726522d7eb8cd8
Bobbythecoder2034 <kkoala110@gmail.com>
2026-05-07 13:24:47 -0700

    swapping to front end
    nothing

--- app/products/Page.jsx
+++ app/products/Page.jsx
@@ -1,36 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
 
-
-const page = () => {
+const Page = () => {
   return (
-    <>
-    <div className="search-area">
-      <button className="search-button">Search</button>
-      <input type='search' className='search-bar' />
-      <select className='filter-bar' >
-        <option value="">Popular</option>
-        <option value="">Download</option>
-        <option value="">Print Count</option>
-        <option value="">Alphabetical</option>
-      </select>
+    <div>
+      
     </div>
-
-    <div className="products">
-      <div className='product'>
-        <div className="product-header">
-          <h1>Mini Submarine</h1>
-        </div>
-        <Image src="" alt="" className="product-img" />
-        <div className="product-footer"></div>
-      </div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    </>
   )
 }
 
-
-export default page
+export default Page
