61b1ab0a16be0a4b5476a6c772282d5a11db2ff9
Robbo00 <rscrog606@west-mec.org>
2026-05-11 15:01:01 -0700

    Product page 85% finished

--- app/products/Page.jsx
+++ app/products/Page.jsx
@@ -8,11 +8,11 @@ const Page = () => {
   
   return (
     <>
-    <div className="w-screen h-[8vh] bg-[#DC965A] flex flex-row items-center justify-evenly p-[.6rem]">
-      <button className='w-[8%] h-[95%] text-decoration-line'>Filter</button>
+    <div className="relative w-screen h-[8vh] bg-[#DC965A] flex flex-row items-center justify-evenly p-[.6rem]">
+      <button className='w-[3%] bg-[#242325] h-full underline text-[#BBB891] absolute  left-0 top-0 text-decoration-line'>Filter</button>
       <button className='bg-[#BBB891] w-[8%] h-[95%] font-semibold border-2'>Search</button>
       <input className='w-[60%] h-[95%] p-[.4rem] bg-[#C8C8C8]' type="search" name="" id="" placeholder='Search for products...'/>
-      <select className='h-[95%] w-[8%]' name="" id="">
+      <select className=' h-[95%] w-[8%]' name="" id="">
         <option value="">Popular</option>
         <option value="">Download</option>
         <option value="">Print Count</option>
@@ -20,8 +20,15 @@ const Page = () => {
       </select>
     </div>
 
-    <div className='w-screen min-h-[84vh] flex-row flex '>
-    </div>
+    <div className='w-screen relative min-h-[84vh] ] flex-row flex p-6 flex-wrap justify-around bg-[#CED4DB gap-8'>
+      <Product featured={true} name={'Mini Boat'} likes={129} dislikes={29} user={'CrazyPenguin138'} /> 
+      <Product featured={true} name={'Strawhat'} likes={110} dislikes={8}   user={'AngryPenguin124'} /> 
+      <Product featured={false} name={'Gear'} likes={110} dislikes={8}   user={'AngryPenguin124'} /> 
+      <Product featured={false} name={'Chain'} likes={110} dislikes={8}   user={'AngryPenguin124'} /> 
+      <Product featured={false} name={'Apple'} likes={110} dislikes={8}   user={'AngryPenguin124'} /> 
+\    </div>
+
+    
     </>
   )
 }
--- components/Product.jsx
+++ components/Product.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import Image from 'next/image';
+import React, { useState } from 'react'
+import { IoMdThumbsDown,  IoMdThumbsUp} from "react-icons/io";
 
-const Product = ({featured, name, likes, dislikes, price, url}) => {
+
+const Product = ({featured, name, likes, dislikes, user, url}) => {
+  let color
+  if(featured){
+     color = '#e09c64'
+  }
+  else {
+     color = '#D6DEE2'
+  }
+
   return (
-    <div>
+    <div className='border-2 rounded-xl w-[30%] h-[52%] relative flex-col' style={{backgroundColor: color}}>
+      <div className='p-2 w-full h-[15%] flex flex-row relative gap-12 '>
+        <h1 className='text-center h-full w-[29%] text-2xl font-bold text-[#282828]'>{name}</h1>
+
+        <div className='h-full w-[50%]'></div>
+        
+      </div>
+
+      <img src="https://www.3dbenchy.com/wp-content/uploads/2017/11/3DBenchy-A-small-giant-in-the-world-of-3D-printing-v01-1024x576.jpg" className='w-full h-[75%] object- object-cover' alt="" />
       
+      <div className='text-center text-xl font-semibold w-full h-[10%] relative  p-1'>
+        By {user}
+      </div>
     </div>
   )
+  
 }
 
 export default Product
