9ae3291678d88f75007156ca9b14f4817d19a2f5
Montgomery Mcdonald <halfbloodhero1027@gmail.com>
2026-05-12 13:47:11 -0700

    finished Home Page, INIT BlogCard.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,54 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect, useMemo } from "react"
+import Image from "next/image"
+import Link from "next/link"
 import Card from '@/components/Card.jsx'
+import Blog from '@/components/Blog.jsx'
 
-export default function page() {
+
+export default function Page() {
+
+  const [featured, setFeatured] = useState([])
+  const [blog, setBlog] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+
+
+  useEffect(() => {
+    async function fetchBlog(){
+
+      try {
+        setLoading(true);
+      const res = await fetch("/api/featured/");
+      const json = await res.json();
+      console.log(json)
+      if (!json.success) throw new Error(json.error);
+      setFeatured(json.data);
+    } catch {
+      setError("Failed to load featured projects. Is MongoDB running?");
+    } finally {
+      setLoading(false);
+    }
+  }    
+  async function fetchBlog(){
+
+      try {
+        setLoading(true);
+      const res = await fetch("/api/featured/");
+      const json = await res.json();
+      console.log(json)
+      if (!json.success) throw new Error(json.error);
+      setBlog(json.data);
+    } catch {
+      setError("Failed to load trending projects. Is MongoDB running?");
+    } finally {
+      setLoading(false);
+    }
+  }
+  fetchFeatured()
+  fetchBlog()
+  }, []);
+
   const DEFAULTCARD = "bg-[#dc965a] w-100% p-2 m-5"
   return (
    <div className="bg-[#c8c8c8] w-screen min-h-screen">
@@ -11,65 +58,24 @@ export default function page() {
     <div className="bg-[#dc965a] m-5 p-2">
       <h1 className="text-5xl text-[#242325]">Popular</h1>
       <div className="w-full p-2 m-5 flex flex-col lg:flex-row justify-evenly items-evenly">
-      <Card src="https://res.cloudinary.com/drg8btdmp/image/upload/v1725580333/cld-sample-2.jpg" name="Bob" creator="Jeff"/>
       <div>
-      <div className="flex flex-wrap wrap w-full lg:flex-row">
-        <div className="bg-[#b3b3b3] lg:max-w-[20vw] lg:max-h-full p-5 m-5 border-6 border-black">
-          <h1 className="text-6xl">2nd</h1>
-          <Image src={"https://res.cloudinary.com/drg8btdmp/image/upload/v1725580333/cld-sample-2.jpg"} alt="" width="100000" height="100" className="p-2.5 w-full"/>
-        <h1 className="text-5xl">{"Item"} By {"Creator"}</h1>
-        </div>
-      </div>
-      <div className="flex flex-wrap wrap">
-        <div className="bg-[#b3b3b3] lg:max-w-[20vw] lg:max-h-full p-5 m-5 border-6 border-black">
-          <h1 className="text-6xl">3rd</h1>
-          <Image src={"https://res.cloudinary.com/drg8btdmp/image/upload/v1725580333/cld-sample-2.jpg"} alt="" width="100000" height="100" className="p-2.5 w-full"/>
-        <h1 className="text-5xl">{"Item"} By {"Creator"}</h1>
-        </div>
-      </div>
+        
+      <Card src={featured[0].image} name={featured[0].name} creator={featured[0].creator}/>
       </div>
       <div>
 
       <div className="flex flex-wrap wrap">
-        <div className="bg-[#b3b3b3] lg:max-w-[20vw] lg:max-h-full p-5 m-5 border-6 border-black">
-          <h1 className="text-6xl">4th</h1>
-          <Image src={"https://res.cloudinary.com/drg8btdmp/image/upload/v1725580333/cld-sample-2.jpg"} alt="" width="100000" height="100" className="p-2.5 w-full"/>
-        <h1 className="text-5xl">{"Item"} By {"Creator"}</h1>
-        </div>
-      </div>
-      <div className="flex flex-wrap wrap">
-        <div className="bg-[#b3b3b3] lg:max-w-[20vw] lg:max-h-full p-5 m-5 border-6 border-black">
-          <h1 className="text-6xl">5th</h1>
-          <Image src={"https://res.cloudinary.com/drg8btdmp/image/upload/v1725580333/cld-sample-2.jpg"} alt="" width="100000" height="100" className="p-2.5 w-full"/>
-        <h1 className="text-5xl">{"Item"} By {"Creator"}</h1>
-        </div>
+        {featured.slice(1, featured.length()).map((item) => {
+          <Card src={item.image} name={item.name} creator={item.creator}/>
+        })}
       </div>
       </div>
       </div>
     </div>
     <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
-    </div>
-    <div className={DEFAULTCARD}>
-
+      <Link src={`https://localhost:3000/blog/${blog._id}`}>
+        <Blog blog={blog} />
+      </Link>
     </div>
    </div>
   )
